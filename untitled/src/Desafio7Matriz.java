ee82e9cdb6a07e8a9f9a42e6b429c53225883f92
Valmir da Silva <valmirsmc.dev@gmail.com>
2025-08-30 22:49:29 -0300

    Matriz Desafio 7

--- untitled/src/Desafio7Matriz.java
+++ untitled/src/Desafio7Matriz.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-
-public class desafio6Vetor {
+public class Desafio7Matriz {
 
     // Método para calcular a média de um aluno
     static double calcularMedia(double n1, double n2) {
@@ -11,78 +10,84 @@ public class desafio6Vetor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int qtde, i;
+
+
         System.out.println("Quantos alunos deseja cadastrar? ");
-        int qtde = sc.nextInt();
+        qtde = sc.nextInt();
         sc.nextLine();
 
         String[] nome = new String[qtde];
-        double[] nota1 = new double[qtde];
-        double[] nota2 = new double[qtde];
-        double[] media = new double[qtde];
-        int i;
+        double[][] notas = new double[qtde][3];
 
-        //entrada dos dados dos alunos
+        //entrada de dados
         for (i = 0; i < qtde; i++) {
-            System.out.println("Nome do aluno " + (i + 1) + ":");
+            System.out.println("Digite o nome do aluno " + (i + 1) + ":");
             nome[i] = sc.nextLine();
 
-            System.out.println("Nota1: ");
-            nota1[i] = sc.nextInt();
+            System.out.println("Digite a nota 1:");
+            notas[i][0] = sc.nextDouble();
 
-            System.out.println("Nota2: ");
-            nota2[i] = sc.nextInt();
-            sc.nextLine(); // limpar buffer
+            System.out.println("Digite a nota 2:");
+            notas[i][1] = sc.nextDouble();
+            sc.nextLine();
 
-            // Calcular e salvar média
-            media[i] = calcularMedia(nota1[i], nota2[i]);
+            notas[i][2] = calcularMedia(notas[i][0], notas[i][1]);
+
+
         }
-        int aprovados = 0, reprovados = 0, recuperacao = 0;
-        double maiorMedia = Double.MIN_VALUE;
-        double menorMedia = Double.MAX_VALUE;
+
+        // Variáveis de estatísticas
+        int aprovados = 0, recuperacao = 0, reprovados = 0;
+        double maiorMedia = Double.MIN_VALUE, menorMedia = Double.MAX_VALUE;
         int idxMaior = -1, idxMenor = -1;
+
 
         //processar resultados
         for (i = 0; i < qtde; i++) {
             // Contagem de aprovados/recuperação/reprovados
-            if (media[i] >= 7) {
+            if (notas[i][2] >= 7) {
                 aprovados++;
-            } else if (media[i] >= 5) {
+            } else if (notas[i][2] >= 5) {
                 recuperacao++;
             } else {
                 reprovados++;
             }
             // Maior e menor média
-            if (media[i] > maiorMedia) {
-                maiorMedia = media[i];
+            if (notas[i][2] > maiorMedia) {
+                maiorMedia = notas[i][2];
                 idxMaior = i;
             }
-            if (media[i] < menorMedia) {
-                menorMedia = media[i];
+            if (notas[i][2] < menorMedia) {
+                menorMedia = notas[i][2];
                 idxMenor = i;
             }
         }
-        //exibir rsultados
-        System.out.println("\n--- Resultados ---");
+
+        System.out.println("-- Resultados --");
         for (i = 0; i < qtde; i++) {
             String status;
-
-            if (media[i] >= 7) {
+            if (notas[i][2] >= 7) {
                 status = "Aprovado";
-            }else if (media[i] >= 5) {
+            } else if (notas[i][2] >= 5) {
                 status = "Recuperação";
             } else {
                 status = "Reprovado";
             }
-            System.out.printf("%s - Média %.1f (%s)\n", nome[i], media[i], status);
-
+            System.out.printf("%-10s - NOTA 1: %-3.1f | NOTA 2: %-3.1f | MEDIA: %-3.1f | %s%n",
+                    nome[i], notas[i][0], notas[i][1], notas[i][2], status);
         }
+
         System.out.println("Aprovados: " + aprovados);
         System.out.println("Recuperação: " + recuperacao);
         System.out.println("Reprovados: " + reprovados);
 
         System.out.printf("Maior média: %s (%.1f)\n", nome[idxMaior], maiorMedia);
         System.out.printf("Menor média: %s (%.1f)\n", nome[idxMenor], menorMedia);
+
+            sc.close();
     }
 
 
 }
+
